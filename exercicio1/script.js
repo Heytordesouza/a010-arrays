6cdc010f188ead0f977175824e8da91c4130b5c7
Heytordesouza <heytordesnascimento@gmail.com>
2022-09-01 13:26:44 -0300

    Exercícios 1,2 e 3 Array

---
+++ exercicio1/script.js
@@ -0,0 +1,17 @@
+//# Exercício 1
+
+//No primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes:
+
+//- O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+const number = [32, 85, 42, 87, 29, 98]
+
+//- O segundo array deve possuir apenas **strings**;
+const arrayString = ["Rio de Janeiro", "Recife", "Porto Alegre", "São Paulo"]
+
+//- O terceiro array deve possuir **números**, **strings** e **booleanos**.
+const arrayGeral = [12, "Natal", 30, true]
+
+//Imprima cada um dos arrays acima, utilizando `console.log()`
+console.log(`${number}
+${arrayString}
+${arrayGeral}`)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,29 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+//# Exercício 2
+
+//Com os arrays criados, agora vamos observar os arrays. Faça o que se pede abaixo, 
+//utilizando `console.log()`:
+const number = [32, 85, 42, 87, 29, 98]
+const arrayString = ["Rio de Janeiro", "Recife", "Porto Alegre", "São Paulo"]
+const arrayGeral = [12, "Natal", 30, true]
+
+//- Imprima a quantidade de itens de cada array (utilize um console.log() para cada impressão);
+console.log(`Quantidade de itens Number: ${number.length}`)
+console.log(`Quantidade de itens String: ${arrayString.length}`)
+console.log(`Quantidade de itens Geral: ${arrayGeral.length}`)
+
+/*- Imprima o primeiro item do primeiro array, o segundo item do segundo array, 
+e o terceiro item do terceiro array;*/
+
+console.log(`Primeiro item de Number: ${number[0]}`)
+console.log(`Segundo item de String: ${arrayString[1]}`)
+console.log(`Terceiro item de Geral: ${arrayGeral[2]}`)
+
+
+/*- Imprima uma informação booleana verdadeira sobre a **inclusão** de um item do primeiro array, 
+e uma informação booleana falsa sobre a **inclusão** de um item ao terceiro array. 
+Isto é, um `includes()` verdadeiro, e outro falso.*/
+
+console.log(`Em number tem o número 42? ${number.includes(42)}`)
+console.log(`Em geral tem a cidade de Manaus? ${arrayGeral.includes("Manaus")}`)
+
+
